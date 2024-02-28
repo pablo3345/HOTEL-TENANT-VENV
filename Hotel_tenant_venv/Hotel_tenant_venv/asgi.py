13f128a801e2bf66b385f84d1be5102494021e03
pablo3345 <pabloandresperuchi@gmail.com>
2024-02-28 18:54:00 -0300

    primera subida env

---
+++ Hotel_tenant_venv/Hotel_tenant_venv/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Hotel_tenant_venv project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Hotel_tenant_venv.settings')
+
+application = get_asgi_application()
